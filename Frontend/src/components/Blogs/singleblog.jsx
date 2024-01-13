3c94921031d30aee98cc65fb7e9fa802a084a292
bibhab <bbobbasnet@gmail.com>
2024-01-13 21:44:43 +0545

    minor changes

--- Frontend/src/components/Blogs/singleblog.jsx
+++ Frontend/src/components/Blogs/singleblog.jsx
@@ -76,16 +76,16 @@ export function SingleBlog() {
     </header>
 
     {/* <!-- Topic Nav --> */}
-    <nav className="w-full py-4 border-t border-b bg-black">
+    <nav className="w-full py-4 border-t border-b bg-gray-100">
                
         <div className="w-full flex-grow sm:flex sm:items-center sm:w-auto">
             <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-                <a href="#" className="hover:bg-gray-400 no-underline text-white hover:underline rounded py-2 px-4 mx-2">Technology</a>
-                <a href="#" className="hover:bg-gray-400 no-underline text-white hover:underline rounded py-2 px-4 mx-2">Automotive</a>
-                <a href="#" className="hover:bg-gray-400 no-underline text-white hover:underline rounded py-2 px-4 mx-2">Finance</a>
-                <a href="#" className="hover:bg-gray-400 no-underline text-white hover:underline rounded py-2 px-4 mx-2">Politics</a>
-                <a href="#" className="hover:bg-gray-400 no-underline text-white hover:underline rounded py-2 px-4 mx-2">Culture</a>
-                <a href="#" className="hover:bg-gray-400 no-underline text-white hover:underline rounded py-2 px-4 mx-2">Sports</a>
+                <a href="#" className="hover:bg-gray-400 no-underline text-black hover:underline rounded py-2 px-4 mx-2">Technology</a>
+                <a href="#" className="hover:bg-gray-400 no-underline text-black hover:underline rounded py-2 px-4 mx-2">Automotive</a>
+                <a href="#" className="hover:bg-gray-400 no-underline text-black hover:underline rounded py-2 px-4 mx-2">Finance</a>
+                <a href="#" className="hover:bg-gray-400 no-underline text-black hover:underline rounded py-2 px-4 mx-2">Politics</a>
+                <a href="#" className="hover:bg-gray-400 no-underline text-black hover:underline rounded py-2 px-4 mx-2">Culture</a>
+                <a href="#" className="hover:bg-gray-400 no-underline text-black hover:underline rounded py-2 px-4 mx-2">Sports</a>
             </div>
         </div>
     </nav>
