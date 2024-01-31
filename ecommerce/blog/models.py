931a1b3af0df76a2889d17da0a523d937592c1fc
bibhab <officialbibhab@gmail.com>
2024-01-31 09:28:07 +0545

    text styling for blog

--- ecommerce/blog/models.py
+++ ecommerce/blog/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.utils import timezone
 import uuid
+from ckeditor.fields import RichTextField
 # Create your models here.
 class Blog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     author=models.CharField(max_length=50)
-    introduction = models.TextField(default='')
-    body = models.TextField(default='')
-    conclusion = models.TextField(default='')
+    body=RichTextField()
     image = models.ImageField(upload_to='blog/images', default='')
     date=models.DateField(default=timezone.now)
     category = models.CharField(max_length=20,default='Technology')
