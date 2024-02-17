f565f337f8e64c584c17e378860340b6885a6fb0
bibhab <officialbibhab@gmail.com>
2024-02-17 20:52:32 +0545

    ratings added for products

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from userauth.models import User
 import uuid
 from django.conf import settings
 from django.utils import timezone
 from ckeditor.fields import RichTextField
+from django.db.models import Avg
 # Create your models here.
 
 class Product(models.Model):
@@ -18,10 +19,23 @@ class Product(models.Model):
 
     def __str__(self):
         return self.productName
+    
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name="images", on_delete=models.CASCADE)
     image = models.ImageField(upload_to='shop/images', default='')
+
+class Rating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ratings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(1, 6)])
+
+    class Meta:
+        unique_together = ('product', 'user')  # Ensure each user can only rate a product once
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.product.update_rating()  # Update the product's rating after a new rating is added
 
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
